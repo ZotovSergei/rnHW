24016470dce84a3d6e31d5f0ce23e9d8c633a78d
ZotovSergei <47771252+ZotovSergei@users.noreply.github.com>
2022-01-09 13:50:35 +0500

    Module 2 feat 4 (#8)
    * Changed navigation in app
    
    * Added Drawer for DrawerNavigation (part 1)
    
    * Added Screens for Product added, Login to Continue, Choose color.
    Added their to Cart
    
    Co-authored-by: Sergei Zotov <zsv.main@gmail.com>

--- helpers/options.ts
+++ helpers/options.ts
@@ -1,16 +1,26 @@
 import {NativeStackNavigationOptions} from "@react-navigation/native-stack";
 import {Colors} from "../utils/constants";
+import {DrawerNavigationOptions} from "@react-navigation/drawer";
 
-export const Main: NativeStackNavigationOptions = {
+export const Main: DrawerNavigationOptions = {
   headerStyle: {
     backgroundColor: Colors.PacificBlue,
   },
   title: "Ecommerce Store",
   headerTitleAlign: "center",
   headerTintColor: Colors.White,
+  drawerLabel: "Home",
 };
 
 export const Product: NativeStackNavigationOptions = {
+  headerStyle: {
+    backgroundColor: Colors.PacificBlue,
+  },
+  headerTitle: "",
+  headerTintColor: Colors.White,
+};
+
+export const CommonStyleHeader: NativeStackNavigationOptions = {
   headerStyle: {
     backgroundColor: Colors.PacificBlue,
   },
